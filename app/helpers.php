07736a8aa4f4395bc83d248ece170a79550651fc
Mario Basic <mario.basic@outlook.com>
2016-07-21 19:08:32 +0200

    Removed useless stuff.

--- app/helpers.php
+++ app/helpers.php
@@ -4,10 +4,10 @@ if (! function_exists('convert_integer')) {
     /**
      * Converts "1.123,45" to 112345.
      *
-     * @param  string $number
+     * @param  $number
      * @return integer
      */
-    function convert_integer(string $number)
+    function convert_integer($number)
     {
         // Remove . and , from number
         return preg_replace('/[.|,]/', '', $number);
