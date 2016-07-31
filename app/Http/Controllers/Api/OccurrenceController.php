7a5b8cfc4083a7e43ba74a4fbf8e80cd41bd0d97
Mario Basic <mario.basic@outlook.com>
2016-07-31 19:59:07 +0200

    Possible fix for automatic occurs_at change during toggling occurrences options.

--- app/Http/Controllers/Api/OccurrenceController.php
+++ app/Http/Controllers/Api/OccurrenceController.php
@@ -9,36 +9,39 @@ use App\Http\Controllers\Controller;
 
 class OccurrenceController extends Controller
 {
-    public function toggleOffer(Occurrence $occurrence, Request $request)
+    public function toggleOffer($id, Request $request)
     {
         $this->validate($request, [
             'state' => 'required|boolean'
         ]);
 
+        $occurrence = Occurrence::findOrFail($id);
         $occurrence->offer_sent = $request->get('state');
         $occurrence->save();
 
         return $occurrence->offer_sent;
     }
 
-    public function togglePayment(Occurrence $occurrence, Request $request)
+    public function togglePayment($id, Request $request)
     {
         $this->validate($request, [
             'state' => 'required|boolean'
         ]);
 
+        $occurrence = Occurrence::findOrFail($id);
         $occurrence->payment_received = $request->get('state');
         $occurrence->save();
 
         return $occurrence->payment_received;
     }
 
-    public function toggleReceipt(Occurrence $occurrence, Request $request)
+    public function toggleReceipt($id, Request $request)
     {
         $this->validate($request, [
             'state' => 'required|boolean'
         ]);
 
+        $occurrence = Occurrence::findOrFail($id);
         $occurrence->receipt_sent = $request->get('state');
         $occurrence->save();
 
